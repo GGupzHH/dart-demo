aa2c06edaad06faa0b482a905bf69384b051e437
赵雅国 <1248023357@qq.com>
2022-06-30 23:10:07 +0800

    feat: dart study list and set

--- demo-01.dart
+++ demo-01.dart
@@ -22,14 +22,54 @@ void typeConversion() {
   print(piAsString == '1.1');
 }
 
+void listStudy() {
+  var list = [1, 2, 3];
+  var list2 = ['a', ...list];
+  // 当list可能为null的时候可以这样写
+  var list3 = ['1', ...?list];
+  print(list);
+  print(list2);
+  print(list3);
 
+  var list4 = [...list, for (var i in list2) '$i'];
+  var list5 = [if (list.length >= 3) '长度大于等于3'];
+  print(list4);
+  print(list5);
+}
+
+void setStudy() {
+  var set1 = {'a', 'b', 'c'};
+  print(set1);
+
+  // 定义set类型
+  var set2 = <String>{};
+  print(set2);
+
+  // 添加一个值,add方法返回bool值
+  set2.add('value');
+  // 添加一个set， 可以添加自己，但是由于set的特性所以没有加重复的值
+  set2.addAll(set1);
+  print(set2);
+  print(set2.length);
+
+  // 创建一个set常量
+  const set3 = <String>{'1'};
+  // set3.add('const定义是常量, 无法再去更改值');
+  print(set3);
+
+  // 同样支持 ... 和 ...? for if
+  final set4 = <String>{...set2, if (set2.length > 2) 'set2.length > 2'};
+  print(set4);
+}
 
 void main() {
   final number = 43;
 
   const foo = const [];
-  print(foo);
+  // print(foo);
 
-  typeConversion();
-  printInteger(number);
+  // typeConversion();
+  // printInteger(number);
+  // listStudy();
+  setStudy();
 }
